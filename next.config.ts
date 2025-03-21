16033283ece2c77a2248a53c91735b256b18ff0e
Mursel <mursal.haxverdiyev@gmail.com>
2025-03-22 01:30:26 +0400

    turbopack added

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  devIndicators: {
-    appIsrStatus: false,
-  },
+  devIndicators: false,
   images: {
     remotePatterns: [
       {
